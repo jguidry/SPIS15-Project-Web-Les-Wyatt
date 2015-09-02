c31c7c6d65dd8e9021f0f2268d8b7786a258adcc
Wyatt Guidry <jguidry@ucsd.edu>
2015-09-02 14:56:20 -0700

    turned off debug

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=1113,debug= True)
+    app.run(host="0.0.0.0",port=1114,debug= False)
 
 
 #
